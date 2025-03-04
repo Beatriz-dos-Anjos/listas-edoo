9ffbb8287af9efbe71a103bbd3407855eeb81e5b
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-03-04 16:26:30 -0300

    feat: added whole program completed

--- lista2/q01/main.cpp
+++ lista2/q01/main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class HashTable
 {
 private:
-    int HashFunction(string key_string);
-    int maxSize;
-    string *hashTableStructure; // estrutura da tabela hash
+    int maxSize;                         // tamanho maximo de itens que estao a ser colocados na tabela hash
+    string *hashTableStructure;          // estrutura da tabela hash
+    int HashFunction(string key_string); // funcao de espelhamento
 
 public:
-    HashTable(int maxSize, string *hashTableStructure);
-    ~HashTable();
-    void insert(string key_string);
-    void remove(string key_String);
-    int find(string key_string);
-};
+    HashTable(int size);
 
-HashTable::HashTable(int maxSize, string *hashTableStructure) // construtor que inicializa o tamanho máximo da tabela, sendo esse 101
-{
-    maxSize = 101;
-    hashTableStructure = new string[maxSize](); // criação da estrutura da tabela hash que possui o tamanho inicializado acima
-}
-HashTable::~HashTable()
-{
-    delete[] hashTableStructure;
-} // destrutor
+    ~HashTable();
+    // funcoes
+    void insert(string key_string);
+
+    void remove(string key_string);
+
+    int find(string key_string);
+
+    string getData(int index); // encapsulamento de dados para permitir o acesso ao atributo privado hashTableStructure
+};
 
 int HashTable::HashFunction(string key_string)
 {
@@ -38,67 +36,143 @@ int HashTable::HashFunction(string key_string)
         h = h + finalResult; // variavel h que guarda todos o valores ascii
         i++;
     }
-    return ((h * 19) % 101);
+    return (h * 19) % maxSize;
+}
+
+HashTable::HashTable(int size)
+{
+    maxSize = size;
+    hashTableStructure = new string[maxSize]; // alocacao dinamica de memoria para a tabela hash
+    for (int i = 0; i < maxSize; i++)
+    {
+        hashTableStructure[i] = ""; // inicializando a tabela hash com valores vazios
+    }
+}
+
+HashTable::~HashTable()
+{
+    delete[] hashTableStructure; // liberando a memoria alocada
 }
 
 void HashTable::insert(string key_string)
 {
+    if (find(key_string) != -1)
+        return; // mais abaixo ha um if que verifica se a chave ja existe, se sim, retorna -1 para nao ter risco de duplicacao
+
     int position = HashFunction(key_string); // calcula a posição da chave na tabela hash baseada na string recebida
     int j = 1;
-    while (hashTableStructure[position] != "" && j < 20) // se a posicao ja tiver ocupada, ou seja, algum dentre o 0 a 100
-    {
 
-        position = ((HashFunction(key_string) + j * j + 23 * j) % 101); // se a posicao ja tiver ocupada, ou seja, algum dentre o 0 a 100
+    while (hashTableStructure[position] != "" && hashTableStructure[position] != "DELETED" && j < 20) // insercao de uma chave na tabela hash
+    {
+        position = (position + j * j + 23 * j) % maxSize; // recalcula a posicao da chave na tabela hash
         j++;
     }
-    if (hashTableStructure[position] == key_string)
-    {
-        return;
-    }
-    else
-    {
-        if (j == 20)
-            return;
 
-        hashTableStructure[position] = key_string; // insere a chave na posicao calculada
+    if (j < 20)
+    {
+        hashTableStructure[position] = key_string;
     }
 }
 
 void HashTable::remove(string key_string)
 {
-    int position = HashFunction(key_string); // posicao inicial da chave string na tabela int
-    int j = 1;
-    while (hashTableStructure[position] != "" && j < 20)
-    {
-        if (hashTableStructure[position] == key_string)
-        {
-            hashTableStructure[position] = "DELETED";
-            return;
-        }
-        position = (position + j * j + 23 * j) % 101;
-        j++;
-    }
+    int position = find(key_string);
+    if (position == -1) // usa o metodo find para averiguar se a chave existe
+        return;
+
+    hashTableStructure[position] = "DELETED"; // se a chave existe, remove ela da tabela hash marcado por deleted
 }
 
 int HashTable::find(string key_string)
 {
-    int position = HashFunction(key_string);
-    if (hashTableStructure[position] == key_string)
+    int position = HashFunction(key_string); // nova posicao na tabela hash
+    int j = 1;
+
+    while (j < 20)
     {
-        return position;
-    }
-    else
-    {
-        int j = 1;
-        while ((hashTableStructure[position] != "" && hashTableStructure[position] != "DELETED") && j < 20)
+        if (hashTableStructure[position] == key_string)
         {
-            if (hashTableStructure[position] == key_string)
-            {
-                return position;
-            }
-            position = (position + j * j + 23 * j) % 101;
-            j++;
+            return position; // se a chave for a mesma que esta sendo procurada, retorna a posicao
         }
-        return -1;
+        if (hashTableStructure[position] == "")
+        {
+            return -1; // se nao encontrou, retorna -1
+        }
+        position = (position + j * j + 23 * j) % maxSize;
+        j++;
     }
+    return -1;
+}
+
+string HashTable::getData(int index)
+{
+    return hashTableStructure[index];
+}
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t > 0)
+    {
+        HashTable hashTable(101); // criacao de uma tabela hash com 101 posicoes
+
+        int n;
+        cin >> n;
+
+        for (int j = 0; j < n; j++)
+        {
+            string operation;
+            cin >> operation;
+            string instructor = "";
+            string key = "";
+            bool foundSignal = false;
+            for (char c : operation) // separacao da string recebida em duas partes, a instrucao e a chave
+            {
+                if (c == ':')
+                {
+                    foundSignal = true; // flag para indicar que o separador foi encontrado :
+                }
+                else if (!foundSignal) // caso contrario , adiciona a instrucao
+                {
+                    instructor += c;
+                }
+                else 
+                {
+                    key += c;
+                }
+            }
+
+            if (instructor == "ADD")
+            {
+                hashTable.insert(key);
+            }
+            else if (instructor == "DEL")
+            {
+                hashTable.remove(key);
+            }
+        }
+
+        int numCount = 0;
+        for (int i = 0; i < 101; i++)
+        {
+            string value = hashTable.getData(i);
+            if (value != "" && value != "DELETED")
+            {
+                numCount++;
+            }
+        }
+        cout << numCount << endl;
+
+        for (int i = 0; i < 101; i++)
+        {
+            string value = hashTable.getData(i);
+            if (value != "" && value != "DELETED")
+            {
+                cout << i << ":" << value << endl;
+            }
+        }
+        t--;
+    }
+    return 0;
 }
